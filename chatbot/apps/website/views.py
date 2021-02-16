3d5eae95ee77d39d5b6c87bdba55226358c1f444
thesorkamax001 tgbh <thesorkamax001@gmail.comgit config --global user-name PabloCJ02>
2021-02-15 21:43:56 -0600

    commit final

--- chatbot/apps/website/views.py
+++ chatbot/apps/website/views.py
@@ -4,17 +4,19 @@ from apps.website.models import Comida
 
 
 def index_website(request):
+    return render(request, 'base.html')
+  
+    Comida.objects.create(nombre='Tacos')
+    comida_lista = Comida.objects.all()
 
-    #Comida.objects.create(nombre='Tacos')
-   # comida_lista = Comida.objects.all()
-    #
-   # for comida in comida_lista:
-   #     print(comida.nombre)
+    for comida in comida_lista:
+        print(comida.nombre)
 
    #tacos = Comida.objects.get(id=1)
    #tacos.nombre = 'tacos de asada'
    #tacos.save()
 
-   Comida.objects.get(id=1).delete()
+   #Comida.objects.get(id=1).delete()
    
-   return render(request, 'base.html')
+   
+
