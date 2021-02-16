4719d4ea1cfe08a211772ab52ddd2c36d9120c13
thesorkamax001 tgbh <thesorkamax001@gmail.comgit config --global user-name PabloCJ02>
2021-02-15 20:42:14 -0600

    Solucionando errores

---
+++ chatbot/apps/website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from apps.website.models import Comida
+# Create your views here.
+
+
+def index_website(request):
+
+    #Comida.objects.create(nombre='Tacos')
+   # comida_lista = Comida.objects.all()
+    #
+   # for comida in comida_lista:
+   #     print(comida.nombre)
+
+   #tacos = Comida.objects.get(id=1)
+   #tacos.nombre = 'tacos de asada'
+   #tacos.save()
+
+   Comida.objects.get(id=1).delete()
+   
+   return render(request, 'base.html')
